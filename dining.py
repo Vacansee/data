b06413d162aac57ca0b413a2cb842950d05b1c89
Adam Tuhacek <105731764+AdamTuhacek@users.noreply.github.com>
2023-12-05 18:17:29 -0500

    Scraper update, use bs4

--- dining.py
+++ dining.py
@@ -9,6 +9,7 @@ url = 'https://rpi.sodexomyway.com/dining-near-me/hours'
 
 import urllib.request
 import json
+from bs4 import BeautifulSoup
 
 # no spaces, replace non standard chars, use numbers as index from days
 #   starting with sunday = 0
@@ -38,63 +39,71 @@ header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) '
       'Accept-Language': 'en-US,en;q=0.8',
       'Connection': 'keep-alive'}
 
-req = urllib.request.Request(url, headers=header)
-page = urllib.request.urlopen(req)
-page = page.read().decode("utf8")
-
-test = page.split('<div class="dining-block">')
-
 rename = {
 
 }
 
-data = dict()
+def write_json():
 
-daystoletter = {'Monday': 'M', 'Tuesday': 'T', 'Wednesday': 'W', 'Thursday': 'R', 'Friday': 'F', 'Saturday': 'Sa', 'Sunday': 'Su'}
-daystonumber = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 0}
-
-for i in range(1,len(test)):
-    name = test[i].split('</a>')[0].split('>')[-1]
-    reghours = test[i].split('<h3>Regular Hours</h3>')[-1].split('arrayregdays')
+    req = urllib.request.Request(url, headers=header)
+    page = urllib.request.urlopen(req)
+    page = page.read().decode("utf8")
     
-    name = name.replace('’',"'").replace('é','e').split(" - ")[-1]
-    if name == "The Commons Dining Hall": name = "Commons Dining Hall"
-    print(name)
-
-    data[name] = dict()
-    # for day in daystoletter:
-    #     #data[name][day] = dict()
-    #     data[name][daystonumber[day]] = []
+    data = dict()
     
-    specmessage = False
-    if test[i].find('<div class="spechours hide">') != -1:
-        specmessage = True
-        special_message = test[i].split('<div class="spechours hide"><div><h3>')
+    daystoletter = {'Monday': 'M', 'Tuesday': 'T', 'Wednesday': 'W', 'Thursday': 'R', 'Friday': 'F', 'Saturday': 'Sa', 'Sunday': 'Su'}
+    daystonumber = {'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 0}
+    
+    soup = BeautifulSoup(page, 'html.parser')
+    
+    with open('test.html', 'w') as f:
+        f.write(soup.prettify())
         
-    for j in range(1, len(reghours)):
-        note = ''
-        if reghours[j].find('<p class="dining-block-note">') != -1:
-            note = reghours[j].split('<p class="dining-block-note">')[-1].split('</p>')[0]
-            note = note.strip(' :-"')
-        days = reghours[j].split('class=')[0].strip('"= ').split(',')
-        hours = reghours[j].split('<p class="dining-block-hours">')[-1].split('</p>')[0]
-        hours = hours.split(' - ')
+    results = soup.find("ul", {"style" : "list-style-type:none;"})
+    
+    locations = results.findAll("div", {"class" : "dining-block"})
+    
+    for location in locations:
+        name = location.find('a').string
+        name = name.replace('’',"'").replace('é','e').split(" - ")[-1]
+        if name == "The Commons Dining Hall": name = "Commons Dining Hall"
+    
+        print(name)
+        data[name] = dict()
         
-        for day in days:
-
-            if len(hours) != 2: continue
-
-            #data[name][daystonumber[day]].append([hours, note])
-
-            start = convert_mil(hours[0])
-            end = convert_mil(hours[1])
-            data[name]["{}:{}-{}:{}".format(daystonumber[day], start, daystonumber[day], end)] = note
-
-        daysstr = ''
-        for day in days:
-            daysstr += daystoletter[day]
-
-        print("\t{}: {} {}".format(daysstr,hours, note))
-
-with open('data/dining.json', 'w') as f:
-    f.write(json.dumps(data, indent = 4))
+        regtime = location.find("div", {"class" : "reghours"})
+        regdays = regtime.findAll("dt", {"class" : "dining-block-days"})
+        #reghours = regtime.findAll("span", {"class" : "dining-block-hours"})
+        info = regtime.findAll("dd")
+        
+        for i in range(len(regdays)):
+            regday = regdays[i]['data-arrayregdays'].split(',')
+            #reghour = reghours[i].string
+            
+            reghour2 = info[i].find("span", {"class" : "dining-block-hours"}).string
+            
+            note_elem = info[i].findAll("span", {"class": "dining-block-note"})
+            if len(note_elem) == 0:
+                note = note = ''
+            else:
+                note = note_elem[0].string.strip(':')
+                
+            print(reghour2)
+            if reghour2 == 'Closed':
+                continue
+                            
+            reghour2 = reghour2.split(' - ')
+            
+            start = convert_mil(reghour2[0])
+            end = convert_mil(reghour2[1])
+    
+            for day in regday:
+                
+                print("\t{}:{}-{}:{} {}".format(daystonumber[day], start, daystonumber[day], end, note))
+    
+                data[name]["{}:{}-{}:{}".format(daystonumber[day], start, daystonumber[day], end)] = note
+    
+    with open('data/dining.json', 'w') as f:
+        f.write(json.dumps(data, indent = 4))
+        
+write_json()
