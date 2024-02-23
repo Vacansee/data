e7cba84b3689c97aca52efcbe78a1f0c74b72664
Adam Tuhacek <105731764+AdamTuhacek@users.noreply.github.com>
2024-02-23 16:35:10 -0500

    Format json, run for all dining halls

--- dining.py
+++ dining.py
@@ -25,6 +25,8 @@ rename = {
 
 }
 
+exclude = ["Have A Nice Day!"]
+
 existing_meal_names = ['brunch', 'dinner', 'lunch', 'breakfast']
 
 def convert_mil(hours):
@@ -102,96 +104,113 @@ def get_hours():
     with open('data/dining.json', 'w') as f:
         f.write(json.dumps(data, indent = 4))
         
+"""
+    Commons: https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15465&locationId=76929001&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall
+    Russell sage: https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15285&locationId=76929002&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall
+    BARH: https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15286&locationId=76929003&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall
+    Blitman: https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15288&locationId=76929015&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall
+"""
+        
 def get_menu_options():
     
-    urls = [
-        
-    ]
+    urls = {
+        "Commons": "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15465&locationId=76929001&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall",
+        "Russell Sage": "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15285&locationId=76929002&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall",
+        "BARH": "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15286&locationId=76929003&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall",
+        "Blitman": "https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15288&locationId=76929015&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall"
+    }
     
     data = dict()
     
-    url = 'https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15465&locationId=76929001&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall'
-    
-    req = urllib.request.Request(url, headers=header)
-    page = urllib.request.urlopen(req)
-    page = page.read().decode("utf8")
-    
-    soup = BeautifulSoup(page, 'html.parser')
-    
-    #For testing, use this to view scraped html
-    with open('test.html', 'w', encoding="utf-8") as f:
-        f.write(soup.prettify())
+    for dininghallname in urls:
         
-    print("================================\n\n\n")
-    print("DONE WITH REQUEST!!")
+        url = urls[dininghallname]
         
-    # To find day:
-    # <div class="bite-day-menu" id="menuid-30-day">, where day = day of month
-    
-    # Under a day, there are multiple blocks
-    # <div class="accordion-block brunch">
-    
-    # List of menu items in <ul aria-describedby="course-ff03fb20b5" class="bite-menu-item">
-    
-    #Gets all 7 days of the current week
-    menu_day = soup.findAll("div", {"class": "bite-day-menu"})
-    
-    #menu_day = menu_day[0:1] Only get first day
-    
-    for day in menu_day:
+        req = urllib.request.Request(url, headers=header)
+        page = urllib.request.urlopen(req)
+        page = page.read().decode("utf8")
         
-        day_num = day.attrs['id']
+        soup = BeautifulSoup(page, 'html.parser')
         
-        data[day_num] = dict()
-        
-        print(day_num)
-        
-        #day = menu_day[0] #only for testing
-        
-        meals = day.findAll("div", {"class": "accordion-block"})
-        
-        for meal in meals:
-            #meal = meals[0] #only for testing
-            meal_name = meal.attrs['class'][-1]
-            data[day_num][meal_name] = dict()
-            print(meal_name)
+        #For testing, use this to view scraped html
+        with open('test.html', 'w', encoding="utf-8") as f:
+            f.write(soup.prettify())
             
-            if meal_name not in existing_meal_names:
-                print("Parsing error! Check meal name")
-                assert(True==False) #delete this later
-                
-            courses = meal.findAll("div", {"class": "bite-menu-course"})
-            menus = meal.findAll('ul', {"class": "bite-menu-item"})
+        print("================================\n\n\n")
+        print("DONE WITH REQUEST!!")
             
-            # Courses represents the name of the station (eg, bakery)
-            # Menus is the list of menu offerings for that station, and is not
-            # nested, so we have to search both and loop through them together.
-            # Therefore each menu corresponds to a course (station) so the num should be equal
-            assert(len(courses) == len(menus))
-
-
-            for i in range(len(courses)):
-                coursename = courses[i].find('h5').text
-                data[day_num][meal_name][coursename] = dict()
-                print("\t\t",courses[i].find('h5').text)
-                menu_items = menus[i].findAll('li')
+        # To find day:
+        # <div class="bite-day-menu" id="menuid-30-day">, where day = day of month
+        
+        # Under a day, there are multiple blocks
+        # <div class="accordion-block brunch">
+        
+        # List of menu items in <ul aria-describedby="course-ff03fb20b5" class="bite-menu-item">
+        
+        #Gets all 7 days of the current week
+        menu_day = soup.findAll("div", {"class": "bite-day-menu"})
+        
+        #menu_day = menu_day[0:1] Only get first day
+        
+        
+        data[dininghallname] = dict()
+        
+        for day in menu_day:
+            
+            day_num = day.attrs['id'].split('-')[1]
+            
+            data[dininghallname][day_num] = dict()
+            
+            print(day_num)
+            
+            #day = menu_day[0] #only for testing
+            
+            meals = day.findAll("div", {"class": "accordion-block"})
+            
+            for meal in meals:
+                #meal = meals[0] #only for testing
+                meal_name = meal.attrs['class'][-1]
+                data[dininghallname][day_num][meal_name] = dict()
+                print(meal_name)
                 
-                for item in menu_items:
+                if meal_name not in existing_meal_names:
+                    print("Parsing error! Check meal name")
+                    assert(True==False) #delete this later
                     
-                    info = item.findAll('a')
-                    item_name = info[0].text
-                    
-                    data[day_num][meal_name][coursename][item_name] = []
-                    print("\t\t\t",item_name)
-                    
-                    for j in range(1, len(info)):
-                        data[day_num][meal_name][coursename][info[0].text].append(info[j].text)
-                        
-                    allergens = item.findAll("img")
-                    for allg in allergens:
-                        data[day_num][meal_name][coursename][info[0].text].append(allg.attrs['alt'])
-                        
-    with open('data/menus.json', 'w') as f:
-        f.write(json.dumps(data, indent = 4))
+                courses = meal.findAll("div", {"class": "bite-menu-course"})
+                menus = meal.findAll('ul', {"class": "bite-menu-item"})
+                
+                # Courses represents the name of the station (eg, bakery)
+                # Menus is the list of menu offerings for that station, and is not
+                # nested, so we have to search both and loop through them together.
+                # Therefore each menu corresponds to a course (station) so the num should be equal
+                assert(len(courses) == len(menus))
     
+    
+                for i in range(len(courses)):
+                    coursename = courses[i].find('h5').text
+                    data[dininghallname][day_num][meal_name][coursename] = dict()
+                    print("\t\t",courses[i].find('h5').text)
+                    menu_items = menus[i].findAll('li')
+                    
+                    for item in menu_items:
+                        
+                        info = item.findAll('a')
+                        item_name = info[0].text
+                        
+                        if item_name in exclude:
+                            continue
+                        
+                        data[dininghallname][day_num][meal_name][coursename][item_name] = []
+                        print("\t\t\t",item_name)
+                        
+                        for j in range(1, len(info)):
+                            data[dininghallname][day_num][meal_name][coursename][info[0].text].append(info[j].text)
+                            
+                        allergens = item.findAll("img")
+                        for allg in allergens:
+                            data[dininghallname][day_num][meal_name][coursename][info[0].text].append(allg.attrs['alt'])
+                            
+        with open('data/menus.json', 'w') as f:
+            f.write(json.dumps(data, indent = 4))
     
