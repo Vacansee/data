3b53f4b2fc1c46cedad19320b3f1bedd6b79000f
Adam Tuhacek <105731764+AdamTuhacek@users.noreply.github.com>
2024-03-15 22:47:09 -0400

    Update printers.py

--- printers.py
+++ printers.py
@@ -34,9 +34,9 @@ def generate_json():
     soup = BeautifulSoup(page, 'html.parser')
     driver.quit()
     
-    # For testing, use this to view scraped html
-    with open('test.html', 'w', encoding="utf-8") as f:
-        f.write(soup.prettify())
+    # For testing, uncomment and use this to view scraped html
+    # with open('test.html', 'w', encoding="utf-8") as f:
+    #     f.write(soup.prettify())
         
             
     table = soup.findAll("table")
@@ -72,57 +72,5 @@ def generate_json():
         printers[building][room].append([printer_id, paper_type, dpi, color, duplex])
         i =+ 1
     
-    with open('printers.json', 'w') as convert_file:
+    with open('data/printers.json', 'w') as convert_file:
          convert_file.write(json.dumps(printers))
-        
-    return
-
-    
-    start = page.find('<strong>Printer List</strong>')
-    end = page.find('Last Reviewed')
-    
-    contents = page[start:end]
-    contents = contents.split('</tr>')
-    
-
-
-
-    
-    
-    if page.find('<div class="l-padded-bottom captcha-container">') != -1 and len(contents) == 1:
-        print("Captcha Error")
-    
-    for i in range(1, len(contents)-1):
-        # print(i)
-        # print(contents[i])
-    
-        color = duplex = False
-    
-        printer_info = contents[i].split('</td>')
-    
-        building = printer_info[0].split('<td>')[-1]
-        if building in rename:
-            building = rename[building]
-        building = building.replace(' ','_')
-    
-        room = printer_info[1].split('<td>')[-1]
-        room = room.replace('<br>', ' ')
-    
-        if printer_info[3].split('<td>')[-1] != '&nbsp;':
-            color = True
-    
-        printer_id = printer_info[2].split('<td>')[-1]
-    
-        paper_type = printer_info[4].split('<td>')[-1]
-        paper_type = paper_type.replace('\u2033', '').replace('\u00d7', 'x').replace('<br>', ' ')
-        
-        if printer_info[5].split('<td>')[-1] != '&nbsp;':
-            duplex = True
-    
-        dpi = printer_info[6].split('<td>')[-1]
-        dpi = dpi.split()[0]
-    
-        with open('printers.json', 'w') as convert_file:
-             convert_file.write(json.dumps(printers))
-        
-        driver.quit()
